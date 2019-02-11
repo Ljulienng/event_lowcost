4949055ac4faf73d338055f26cb6add2f9e9ff2e
Ljulienng <ljuliennguyen@gmail.com>
2019-02-11 13:28:45 +0100

    RAS

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,12 @@ class Event < ApplicationRecord
 	has_many :users, through: :attendances
 	belongs_to :admin, class_name: "User"
 
-	validates :start_date,
-		presence: true,
+	validates :start_date,presence: true
 
 	validates :duration,
 		presence: true,
+		if: :{multiple_of_5}:true
+
 
 	validates :title,
 		presence: true,
@@ -19,8 +20,17 @@ class Event < ApplicationRecord
 
 	validates :price,
 		presence: true,
-	validates_inclusion_of :price, :in => 1..1000
+		inclusion: {in: 1..1000}
 
 	validates :location, presence: true
+
+
+  def multiple_of_5
+    if duration % 5 == 0
+      return true
+    else  
+      return false
+    end
+  end
 
 end
