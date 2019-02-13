45a2275033958222e6d3c23ad54a93832f149006
Arbousied <kiger.maxence@gmail.com>
2019-02-13 14:15:19 +0100

    yes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,18 +65,18 @@ class EventsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def end_date
+    @event = Event.find(params[:id])
+    ((@event.duration / 60)+0.5).to_i = k
+    end_daate = @event.start_date + k.hour
+    return end_daate
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
-    end
-
-    def end_date
-      @event = Event.find(params[:id])
-      ((@event.duration / 60)+0.5).to_i = k
-      end_daate = @event.start_date + k.hour
-      return end_daate
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
