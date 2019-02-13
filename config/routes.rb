e5a8d8b915e94b364b56c2ca99ab92e0b2c767ef
Ljulienng <ljuliennguyen@gmail.com>
2019-02-13 12:09:45 +0100

    RAS

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/secret'
   devise_for :users
   resources :users
+  resources :charges
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
