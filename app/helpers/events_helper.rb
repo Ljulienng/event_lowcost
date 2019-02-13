a5bd6d8e93e965b46e42e4023db6af4b88759583
Arbousied <kiger.maxence@gmail.com>
2019-02-13 15:09:48 +0100

    yes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,8 +2,8 @@ module EventsHelper
 
 	def end_date
     @event = Event.find(params[:id])
-    k = ((@event.duration / 60)+0.5).to_i
-    end_daate = @event.start_date + k.hour
+    k = (@event.duration).to_i
+    end_daate = @event.start_date + k.minutes
     return end_daate
   end
 
