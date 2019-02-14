1183338ff0558dbab8d50b760e6c40dfaa49c8c7
Ljulienng <ljuliennguyen@gmail.com>
2019-02-14 08:59:33 +0100

    RAS

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :events do
   	resources :attendances
+  	resources :charges
   end
 	root 'events#index'
   get 'home/index'
   get 'home/secret'
   devise_for :users
   resources :users
-  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
